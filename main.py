3dbd57efd468f6d3ca1cb10ebf769d301f9202f4
garryjable <garryjable@gmail.com>
2019-09-08 11:20:35 -0600

    cleaning up some of the leftover test socket code, deleting tello3 example file

--- main.py
+++ main.py
@@ -4,33 +4,31 @@ import time
 
 def Main():
     host = '127.0.0.1'
-    port = 8890
+    drone_port = 8889
 
-    #server = ('127.0.0.1', 8889)
+    dispatcher = DroneDispatcher(host, drone_port)
 
-    #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    #s.bind((host,port))
-
-    dispatcher = DroneDispatcher(host, port)
-
-    print("Welcome to the tello drone client, press q to quit, or enter 1 2 or 3 to fly a pretend mission, enter test to test command")
+    print("Welcome to the tello drone client, press q to quit, or enter 1 2 or 3 to fly a mission")
     message = input("-> ")
     while message != 'q':
         response = dispatcher.execute_mission(message)
-        print(reponse)
+        print(response)
         message = input("-> ")
-    s.close()
 
 class DroneDispatcher:
 
     def __init__(self, host, port):
         self.host = host 
-        self.server = (self.host, port)
-        self.port = port + 1 
+        self.drone = (self.host, port)
+        self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.bind((host,port))
+        self.socket.bind((host, port + 1))
+        return
+    
+    def __del__(self):
+        self.socket.close()
 
-    def execute_mission(mission_num):
+    def execute_mission(self, mission_num):
         mission_one = ['up 50','left 50','right 100','left 50', 'down 50']
         mission_two = ['up 50','right 50','left 100','right 50', 'down 50']
         mission_three = ['up 25','right 25','left 50','right 25', 'down 25']
@@ -43,18 +41,19 @@ class DroneDispatcher:
             mission = mission_three
         else:
             return "invalid mission"
-        send_command('command')    
-        send_command('takeoff')    
+        self.send_command('command')    
+        self.send_command('takeoff')    
         for command in mission:
-            send_command(command)
-        send_command('land')    
-        return 'you flew mission ' + message
+            self.send_command(command)
+        self.send_command('land')    
+        return 'you flew mission ' + mission_num
     
-    def send_command(command):
-        self.socket.sendto(command.encode(), self.server)
+    def send_command(self, command):
+        self.socket.sendto(command.encode(), self.drone)
         data, addr = self.socket.recvfrom(1024)
         print( 'Recieved from server: ' + str(data.decode()))
         time.sleep(5)
+        return
  
 
 
